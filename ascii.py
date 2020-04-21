6e3b2cfde23d3e989af69421b72966d1aacf5cfc
receyuki <receyuki@gmail.com>
2020-04-22 03:03:38 +1000

    ascii art title hide mac

---
+++ ascii.py
@@ -0,0 +1,46 @@
+ASCII_TITLE = """
+   _____       _  __   _____                _   _          
+  / ____|     | |/ _| |  __ \              | | (_)         
+ | |  __  ___ | | |_  | |__) | __ __ _  ___| |_ _  ___ ___ 
+ | | |_ |/ _ \| |  _| |  ___/ '__/ _` |/ __| __| |/ __/ _ \\
+ | |__| | (_) | | |   | |   | | | (_| | (__| |_| | (_|  __/
+  \_____|\___/|_|_|   |_|   |_|  \__,_|\___|\__|_|\___\___|
+     /\         (_)               | | (_)                  
+    /  \   _ __  _ _ __ ___   __ _| |_ _  ___  _ __        
+   / /\ \ | '_ \| | '_ ` _ \ / _` | __| |/ _ \| '_ \       
+  / ____ \| | | | | | | | | | (_| | |_| | (_) | | | |      
+ /_/    \_\_| |_|_|_| |_| |_|\__,_|\__|_|\___/|_| |_|      
+  ___          _               _   ___                      
+ | _ ) __ _ __| |_____ _ _  __| | / __| ___ _ ___ _____ _ _ 
+ | _ \/ _` / _| / / -_) ' \/ _` | \__ \/ -_) '_\ V / -_) '_|
+ |___/\__,_\__|_\_\___|_||_\__,_| |___/\___|_|  \_/\___|_|  
+
+ ===========================================================            
+                 Project   E-12-SWIL-016
+ ===========================================================
+"""
+
+ASCII_DOGE = """
+                Y.                      _   
+                YiL                   .```.  
+                Yii;                .; .;;`.    
+                YY;ii._           .;`.;;;; :    
+                iiYYYYYYiiiii;;;;i` ;;::;;;;    
+            _.;YYYYYYiiiiiiYYYii  .;;.   ;;; 
+         .YYYYYYYYYYiiYYYYYYYYYYYYii;`  ;;;;    
+       .YYYYYYY$$YYiiYY$$$$iiiYYYYYY;.ii;`..   
+      :YYY$!.  TYiiYY$$$$$YYYYYYYiiYYYYiYYii.    
+      Y$MM$:   :YYYYYY$!"``"4YYYYYiiiYYYYiiYY.    
+   `. :MM$$b.,dYY$$Yii" :'   :YYYYllYiiYYYiYY    
+_.._ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY
+.,._ $b`P`     "4$$$$$iiiiiiii$$$$YY$$$$$$YiY;
+   `,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL
+    "`;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU:  
+    ;$P$;;: ;;;;i$y$"!Y$$$b;$$$Y$YY$$YYYiiiYYiYY 
+    $Fi$$ .. ``:iii.`-":YYYYY$$YY$$$$$YYYiiYiYYY    
+    :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYYiYY:    
+     :$$$$$i;;iiiiidYYYYYYYYYY$$$$$$YYYYYYYiiYYYY. 
+      `$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY    
+      .i!$$$$$$YYYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY    
+     :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'    
+"""
--- transmitter.py
+++ transmitter.py
@@ -1,5 +1,7 @@
 import bluetooth
+import sys
 from sh import bluetoothctl
+
 
 # mbp A4:83:E7:E4:45:DE
 
@@ -12,21 +14,30 @@ class Transmitter:
         self.address = None
 
     def connect(self):
+        # turn on bluetooth
         bluetoothctl("power", "on")
         bluetoothctl("discoverable", "yes")
 
+        # display near by device
         nearby_devices = bluetooth.discover_devices(lookup_names=True)
         print("Found {} devices.".format(len(nearby_devices)))
 
         for addr, name in nearby_devices:
-            print("  {} - {}".format(addr, name))
+            if "-hidemac" in sys.argv:
+                print("  {} - {}".format("**:**:**:**:**:**", name))
+            else:
+                print("  {} - {}".format(addr, name))
 
         port = 1
+        # establish connection
         self.server_sock.bind(("", port))
         self.server_sock.listen(1)
         print("Waiting for in coming connection...")
         self.client_sock, self.address = self.server_sock.accept()
-        print("Accepted connection from ", self.address)
+        if "-hidemac" in sys.argv:
+            print("Accepted connection from ", ["**:**:**:**:**:**", self.address[1]])
+        else:
+            print("Accepted connection from ", self.address)
 
     def send(self, msg):
         try:
