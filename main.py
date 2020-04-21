631473d660ae51375c65a73890cfa535efea4ac8
receyuki <receyuki@gmail.com>
2020-04-22 05:42:15 +1000

    logging

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+# -*- encoding:utf-8 -*-
 import sys
 import ascii
+import logging
 from transmitter import Transmitter
+
+# create logger
+logger = logging.getLogger("server")
+logger.setLevel(logging.DEBUG)
+
+formatter =logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+# create file handler
+fh = logging.FileHandler("server.log")
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+
+# create console handler
+ch = logging.StreamHandler()
+if "-debug" in sys.argv:
+    ch.setLevel(logging.DEBUG)
+else:
+    ch.setLevel(logging.INFO)
+
+if "-nolog" not in sys.argv:
+    logger.addHandler(fh)
+logger.addHandler(ch)
+
+logger.debug("Server running")
+logger.debug("The arguments are: %s", sys.argv[1:])
 
 if "-doge" in sys.argv:
     print(ascii.ASCII_DOGE)
--- transmitter.py
+++ transmitter.py
@@ -1,5 +1,6 @@
 import bluetooth
 import sys
+import logging
 from sh import bluetoothctl
 
 
@@ -9,6 +10,7 @@ from sh import bluetoothctl
 class Transmitter:
 
     def __init__(self):
+        self.logger = logging.getLogger("server.Transmitter")
         self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
         self.client_sock = None
         self.address = None
@@ -17,35 +19,41 @@ class Transmitter:
         # turn on bluetooth
         bluetoothctl("power", "on")
         bluetoothctl("discoverable", "yes")
+        self.logger.debug("Bluetooth on")
 
         # display near by device
         nearby_devices = bluetooth.discover_devices(lookup_names=True)
-        print("Found {} devices.".format(len(nearby_devices)))
+        self.logger.info("Found {} devices.".format(len(nearby_devices)))
 
         for addr, name in nearby_devices:
             if "-hidemac" in sys.argv:
-                print("  {} - {}".format("**:**:**:**:**:**", name))
+                self.logger.info("  {} - {}".format("**:**:**:**:**:**", name))
+                self.logger.debug([addr, name])
             else:
-                print("  {} - {}".format(addr, name))
+                self.logger.info("  {} - {}".format(addr, name))
 
         port = 1
         # establish connection
         self.server_sock.bind(("", port))
         self.server_sock.listen(1)
-        print("Waiting for in coming connection...")
+        self.logger.debug("Sock listening to port %i", port)
+        self.logger.info("Waiting for in coming connection...")
         self.client_sock, self.address = self.server_sock.accept()
         if "-hidemac" in sys.argv:
-            print("Accepted connection from ", ["**:**:**:**:**:**", self.address[1]])
+            self.logger.info("Accepted connection from %s", ["**:**:**:**:**:**", self.address[1]])
+            self.logger.debug(self.address)
         else:
-            print("Accepted connection from ", self.address)
+            self.logger.info("Accepted connection from %s", self.address)
 
     def send(self, msg):
         try:
             self.client_sock.send(msg.encode("utf-8"))
+            self.logger.debug("Send message [%s]", msg)
         except bluetooth.btcommon.BluetoothError as e:
-            print(e)
+            self.logger.warning(e)
             self.client_sock, self.address = self.server_sock.accept()
 
     def close(self):
         self.client_sock.close()
         self.server_sock.close()
+        self.logger.info("Socket close")
